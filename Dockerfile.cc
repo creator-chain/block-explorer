9d5fd09710ef40e01281160e02baaf6e2eb75306
richard <richard@creatorchain.network>
2021-06-03 11:08:55 +0700

    hotfix CI/CD: change folder before build docker

--- Dockerfile.cc
+++ Dockerfile.cc
@@ -1,12 +1,3 @@
-FROM node:12-alpine as builder
-
-WORKDIR /apps
-COPY . .
-
-RUN yarn && NODE_ENV=production yarn build:www
-CMD ["ls", "-al", "build"]
-
-# ===========================================================
 FROM nginx:stable-alpine
 
 # The following is mainly for doc purpose to show which ENV is supported
@@ -19,7 +10,7 @@ COPY env.sh .
 RUN apk add --no-cache bash; chmod +x env.sh
 
 COPY docker/nginx.conf /etc/nginx/nginx.conf
-COPY --from=builder /apps/packages/apps/build /usr/share/nginx/html
+COPY build /usr/share/nginx/html
 
 EXPOSE 80
 
